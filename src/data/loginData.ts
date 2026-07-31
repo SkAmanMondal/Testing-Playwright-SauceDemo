a1285ca519afc9034c27386c6804171018c6b0e8
Sk Aman Mondal <amanmondal05@gmail.com>
2026-07-31 18:07:26 +0530

    feat: add login test data

---
+++ src/data/loginData.ts
@@ -0,0 +1,26 @@
+export const loginData = {
+    validUser: {
+        username: "standard_user",
+        password: "secret_sauce"
+    },
+
+    invalidUsername: {
+        username: "wrong_user",
+        password: "secret_sauce"
+    },
+
+    invalidPassword: {
+        username: "standard_user",
+        password: "wrong_password"
+    },
+
+    invalidCredentials: {
+        username: "wrong_user",
+        password: "wrong_password"
+    },
+
+    lockedUser: {
+        username: "locked_out_user",
+        password: "secret_sauce"
+    }
+};
